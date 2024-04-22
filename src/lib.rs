738825f059ba7ecff8d3c8e449995989d0e1b319
Guillem Arias Fauste <hello@gariasf.com>
2024-04-22 16:46:54 +0200

    simplify implementation with closures and iterators

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use std::fs;
-use std::error::Error;
 use std::env;
+use std::error::Error;
+use std::fs;
 
 pub struct Config {
     pub query: String,
@@ -9,23 +9,30 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+        let query = match args.next() {
+            Some(v) => v,
+            None => return Err("Didn't get a query string"),
+        };
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let file_path = match args.next() {
+            Some(v) => v,
+            None => return Err("Didn't get a file path"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config { query, file_path, ignore_case })
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
-
-pub fn run (config: crate::Config) -> Result<(), Box<dyn Error>> {
+pub fn run(config: crate::Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
     let results = if config.ignore_case {
         search_case_insensitive(&config.query, &contents)
@@ -33,25 +40,17 @@ pub fn run (config: crate::Config) -> Result<(), Box<dyn Error>> {
         search(&config.query, &contents)
     };
 
-    for line in  results {
+    for line in results {
         println!("{line}");
     }
 
     Ok(())
 }
 
-pub fn search<'a>(
-    query: &str,
-    contents: &'a str,
-) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents.lines()
+            .filter(|line| line.contains(query))
+            .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
@@ -67,7 +66,6 @@ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a st
     results
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -80,7 +78,7 @@ Rust:
 safe, fast, productive.
 Pick three.";
 
-        assert_eq!(vec!["safe, fast, productive."], search(query,contents))
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents))
     }
 
     #[test]
@@ -92,7 +90,9 @@ safe, fast, productive.
 Pick three.
 Trust me.";
 
-        assert_eq!(vec!["Rust:", "Trust me."],
-        search_case_insensitive(query, contents));
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
